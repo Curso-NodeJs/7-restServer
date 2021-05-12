efc6325e9fae3eefd06cdb33e075b6a8948ee9dd
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2021-05-11 22:19:22 -0500

    Google Sign Agregado

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ para reconstruir los modulos
 Se debe cambiar .enV en .env y agregar la conexión de la base de datos junto al puerto de ejecución
 
 
-para desplegar en heroku las variables de entorno se usuario
-ejemplo:
-heroku config:set MONGODB_CNN=asdf23452
+# para desplegar en heroku las variables de entorno:
+
+heroku config:set PORT=8000
+heroku config:set MONGODB_CNN=
+heroku config:set SECRETORPRIVATEKEY=
+heroku config:set GOOGLE_CLIENT_ID=
+heroku config:set GOOGLE_SECRET_ID=
+
+# para ver los logs en heroku
+
+heroku logs -n 100 --tail
+
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -2,6 +2,9 @@ const { response, request } = require('express');
 const Usuario = require('../models/usuario.model');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify'); 
+
+
 
 const login = async (req = request, res = response) => {
     const { correo, password } = req.body;
@@ -48,7 +51,53 @@ const login = async (req = request, res = response) => {
     
 }
 
+const googleSignIn = async (req = request, res = response, next)=>{
+    const { id_token } = req.body;
+    try {
+        const { correo, nombre, img } =  await googleVerify(id_token);
+        // validar si correo existe en base de datos
+        let usuario = await Usuario.findOne({ correo });
+        if (!usuario){
+            // si no existe tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: 'NoPassword',
+                img,
+                google: true
+            };
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+        
+        // si el usuario en DB esta desactivado
+        if (!usuario.estado) {
+            return res.status(401).json({
+               ok: false,
+               msg:'Usuario Bloqueado' 
+            });
+        }
+        
+        // Generar JWT
+        console.log(usuario);
+        const token = await generarJWT( usuario.id );
+        
+        return res.json({
+            ok:true,
+            usuario,
+            token
+        })
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg:'Token de Google no es valido',
+            ...error
+            })
+    }
+}
+
 
 module.exports = {
     login,
+    googleSignIn
 }
